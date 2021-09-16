e41d0e62a90f0615c949cf040780265e2e23bb69
dfelicehanlon850 <89938403+dfelicehanlon850@users.noreply.github.com>
2021-09-16 15:35:55 -0400

    Assignment 2
    Assignment 2

---
+++ main.py
@@ -0,0 +1,19 @@
+
+def function():
+#get the users name
+  name = input("Tell me your name: ")
+#print the users name
+  print(f"Hello {name}") 
+  
+#asks the user for a number
+  number = int(input("Enter a number: "))
+#prints the users number after multiplying it by itself
+  print(str(number * number))
+
+#asks the user for a word
+  word = input("Enter a word: ")
+#counts the numer of letters and prints to the screen
+  print(str(len(word)))
+
+function()
+
